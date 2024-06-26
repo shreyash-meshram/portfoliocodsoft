2a00fe0b49d8e626ce8da6faf507cf5ab737d1ea
shreyash-meshram <shreyashinno@gmail.com>
2024-06-26 11:09:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+var typed = new Typed('#element', {
+    strings: ['Coder', 'Problem Solver', 'Web Developer', 'Coder', 'Problem Solver', 'Web Developer', 'Coder', 'Problem Solver', 'Web Developer', 'Coder', 'Problem Solver', 'Web Developer', 'Coder', 'Problem Solver', 'Web Developer', 'Coder', 'Problem Solver', 'Web Developer'],
+    typeSpeed: 90,
+  });
